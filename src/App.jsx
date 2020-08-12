2ef20ca23e60d9da84e52652d8f0de73d98ea00c
Marco Piovanello <marco.piovanello@gmail.com>
2020-08-12 23:30:02 +0200

    ancora un altro commit perché ho scritto true al posto di dark

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ class App extends React.Component {
       latest: [],
       airing: [],
       random: [],
-      theme: localStorage.getItem('theme') || true
+      theme: localStorage.getItem('theme') || 'dark'
     };
     this.changeTheme = this.changeTheme.bind(this);
 
