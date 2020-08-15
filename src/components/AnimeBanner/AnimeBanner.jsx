ed0890fb52aa710d68b150b315a2dba9e196e447
Marco Piovanello <marco.piovanello@gmail.com>
2020-08-15 15:16:33 +0200

    fix titoli eccessivamente lunghi per mobile

--- src/components/AnimeBanner/AnimeBanner.jsx
+++ src/components/AnimeBanner/AnimeBanner.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import { String as Sugar } from 'sugar';
+import globals from '../../globals/variables';
 import './AnimeBanner.scss';
 
 class AnimeBanner extends React.Component {
+
+    truncateIfMobile(prop) {
+        if (window.innerWidth <= globals.MOBILE_MAX_WIDTH) {
+            if (prop.length > 28) {
+                return Sugar.truncate(prop, 28, '...')
+            }
+            return prop;
+        }
+        return prop;
+    }
 
     render() {
         return (
@@ -10,8 +22,8 @@ class AnimeBanner extends React.Component {
                 <div className='banner' style={{ backgroundImage: `url(${this.props.pic})` }}>
                 </div>
                 <Container>
-                    {this.props.text ? <h1 className='banner-text'>{this.props.text}</h1> : null}
-                    {this.props.title ? <h1 className='banner-title'>{this.props.title}</h1> : null}
+                    {this.props.title ? <h1 className='banner-title'>{this.truncateIfMobile(this.props.title)}</h1> : null}
+                    {this.props.text ? <h1 className='banner-text'>{this.truncateIfMobile(this.props.text)}</h1> : null}
                 </Container>
             </div>
         );
