6288ad0c023f50b732560baed7178457c861a6ab
Marco Piovanello <marco.piovanello@gmail.com>
2020-09-18 21:56:59 +0200

    Archivio e calendario da class a functional

--- src/components/AnimeArchive/Archive.jsx
+++ src/components/AnimeArchive/Archive.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Subject } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
 import { switchMap } from 'rxjs/operators';
@@ -10,90 +10,86 @@ import AnimeThumb from '../AnimeThumb/AnimeThumb';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import './Archive.scss';
 
-class Archive extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            animes: []
-        }
-        this.subscription = new Subject();
-    }
+const Archive = (props) => {
+    const [animes, setAnimes] = useState([]);
+    let subscription = new Subject();
 
-    componentDidMount() {
+    const helmetContext = {}
+    const genre = decodeURIComponent(props.location.pathname.substring(10));
+
+    useEffect(() => {
         window.scrollTo(0, 0);
-        const genre = decodeURIComponent(this.props.location.pathname.substring(10));
         if (genre)
-            this.subscription = fromFetch(`${globals.API_URL}anime/genre/${genre}`)
+            subscription = fromFetch(`${globals.API_URL}anime/genre/${genre}`)
                 .pipe(
                     switchMap(res => res.json())
                 )
-                .subscribe(data => this.setState({ animes: data }), e => console.error(e));
-    }
+                .subscribe(data => setAnimes(data), e => console.error(e));
+        return () => subscription.unsubscribe();
+    }, [])
 
-    filterByGenre = (e) => {
+    const filterByGenre = (e) => {
         const { value } = e.target;
-        this.subscription = fromFetch(`${globals.API_URL}anime/genre/${value}`)
+        subscription = fromFetch(`${globals.API_URL}anime/genre/${value}`)
             .pipe(
                 switchMap(res => res.json())
             )
-            .subscribe(data => this.setState({ animes: data }), e => console.error(e));
+            .subscribe(data => setAnimes(data), e => console.error(e));
+        return () => subscription.unsubscribe();
     }
 
-    render() {
-        const _genre = decodeURIComponent(this.props.location.pathname.substring(10));
-        const helmetContext = {}
-        return (
-            <div>
-                <HelmetProvider context={helmetContext}>
-                    <Helmet>
-                        <meta name='language' content='it' />
-                        <meta name="description" content={
-                            `Certe volte è dura ricordarsi istantaneamente il nome di una serie :/ , per questo ti offriamo dei comodi 
+    return (
+        <div>
+            <HelmetProvider context={helmetContext}>
+                <Helmet>
+                    <meta name='language' content='it' />
+                    <meta name="description" content={
+                        `Certe volte è dura ricordarsi istantaneamente il nome di una serie :/ , per questo ti offriamo dei comodi 
                             filtri per la ricerca`
-                        } />
-                        <title>Archivio Anime - ricerca filtrata - AnimeAndromeda - Anime Andromeda</title>
-                        <link rel='canonical' href='https://www.animeandromeda.net' />
-                    </Helmet>
-                </HelmetProvider>
-                <div className='mt-3'></div>
-                <div className='container anime-container p-3 shadow rounded'>
-                    <div className='row'>
-                        <div className='col'>
-                            <h3 className='home-section-title'>{'Archivio Anime'}</h3>
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col'>
-                            <Form.Group as={'div'} controlId="formGridState">
-                                <Form.Control as="select" defaultValue={_genre} onChange={this.filterByGenre}>
-                                    {genresDomain.map((genre, idx) => (
-                                        <option key={idx}>{genre}</option>
-                                    ))}
-                                </Form.Control>
-                            </Form.Group>
-                        </div>
-                    </div>
-                    <div className='row'>
-                        {this.state.animes?.length <= 0 && this.props.location.pathname.substring(16) ?
-                            <div className='col text-center'>
-                                <Spinner animation='grow' className='loader-themed' />
-                            </div> : null
-                        }
-                        {this.state.animes.map((anime, k) => (
-                            <div className='col-6 col-md-3 col-lg-2 mobile-responsive-col' key={k}>
-                                <AnimeThumb
-                                    series={anime._id.series}
-                                    pic={anime.pic}
-                                    thumb={anime.thumb}
-                                    title={anime.pretty}>
-                                </AnimeThumb>
-                            </div>
-                        ))}
+                    } />
+                    <title>Archivio Anime - ricerca filtrata - AnimeAndromeda - Anime Andromeda</title>
+                    <link rel='canonical' href='https://www.animeandromeda.net' />
+                </Helmet>
+            </HelmetProvider>
+            <div className='mt-3'></div>
+            <div className='container anime-container p-3 shadow rounded'>
+                <div className='row'>
+                    <div className='col'>
+                        <h3 className='home-section-title'>{'Archivio Anime'}</h3>
                     </div>
                 </div>
+                <div className='row'>
+                    <div className='col'>
+                        <Form.Group as={'div'} controlId="formGridState">
+                            <Form.Control as="select" defaultValue={genre} onChange={filterByGenre}>
+                                {genresDomain.map((genre, idx) => (
+                                    <option key={idx}>{genre}</option>
+                                ))}
+                            </Form.Control>
+                        </Form.Group>
+                    </div>
+                </div>
+                <div className='row'>
+                    {animes?.length <= 0 && props.location.pathname.substring(16) ?
+                        <div className='col text-center'>
+                            <Spinner animation='grow' className='loader-themed' />
+                        </div> : null
+                    }
+                    {animes?.map((anime, k) => (
+                        <div className='col-6 col-md-3 col-lg-2 mobile-responsive-col' key={k}>
+                            <AnimeThumb
+                                series={anime._id.series}
+                                pic={anime.pic}
+                                thumb={anime.thumb}
+                                title={anime.pretty}>
+                            </AnimeThumb>
+                        </div>
+                    ))}
+                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
 
 export default Archive;
--- src/components/AnimeCalendar/Calendar.jsx
+++ src/components/AnimeCalendar/Calendar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import { Subject } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
@@ -8,145 +8,135 @@ import globals from '../../globals/variables'
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import './Calendar.scss';
 
+const Calendar = () => {
+    const [calendar, setCalendar] = useState([]);
+    let subscription = new Subject();
+    let helmetContext = {}
 
-class Calendar extends React.PureComponent {
-    constructor() {
-        super();
-        this.state = {
-            calendar: {},
-        }
-        this.subscription = new Subject();
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         window.scrollTo(0, 0);
-        this.subscription = fromFetch(globals.API_URL + 'anime/calendar')
+        subscription = fromFetch(`${globals.API_URL}anime/calendar`)
             .pipe(
                 switchMap(res => res.json())
             )
-            .subscribe(data => this.setState({ calendar: data }), e => console.error(e));
-    }
+            .subscribe(data => setCalendar(data), e => console.error(e));
+        return () => subscription.unsubscribe();
+    }, []);
 
-    componentWillUnmount() {
-        this.subscription.unsubscribe();
-    }
-
-    render() {
-        const helmetContext = {}
-        return (
-            <div>
-                <HelmetProvider context={helmetContext}>
-                    <Helmet>
-                        <meta name='language' content='it' />
-                        <meta name="description" content={
-                            `Controlla le prossime uscite delle tue serie preferite su AnimeAndromeda.
+    return (
+        <div>
+            <HelmetProvider context={helmetContext}>
+                <Helmet>
+                    <meta name='language' content='it' />
+                    <meta name="description" content={
+                        `Controlla le prossime uscite delle tue serie preferite su AnimeAndromeda.
                             Sempre in alta definizione e senza pubblicità!`
-                        } />
-                        <title>Calendario Uscite - AnimeAndromeda - Anime Andromeda</title>
-                        <link rel='canonical' href='https://www.animeandromeda.net' />
-                    </Helmet>
-                </HelmetProvider>
-                <div className='mt-3'></div>
-                <div className='container home-anime-container p-3 shadow rounded'>
-                    <div className='row'>
-                        <div className='col'>
-                            <h3 className='home-section-title'>{'Calendario Uscite'}</h3>
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col'>
-                            <Table responsive borderless className='anime-calendar'>
-                                <tbody>
-                                    <tr>
-                                        <th>Lunedì</th>
-                                        {this.state.calendar?.mon?.map((day, idx) => (
-                                            <td key={idx}>
-                                                <Link className='link-calendar' to={`/anime/details/${day.series}`}>
-                                                    <p>{day.title}</p>
-                                                </Link>
-                                                <p>{day.hours.slice(0, -3)}*</p>
-                                            </td>
-                                        ))}
-                                    </tr>
-                                    <tr>
-                                        <th>Martedì</th>
-                                        {this.state.calendar?.tue?.map((day, idx) => (
-                                            <td key={idx}>
-                                                <Link className='link-calendar' to={`/anime/details/${day.series}`}>
-                                                    <p>{day.title}</p>
-                                                </Link>
-                                                <p>{day.hours.slice(0, -3)}*</p>
-                                            </td>
-                                        ))}
-                                    </tr>
-                                    <tr>
-                                        <th>Mercoledì</th>
-                                        {this.state.calendar?.wed?.map((day, idx) => (
-                                            <td key={idx}>
-                                                <Link className='link-calendar' to={`/anime/details/${day.series}`}>
-                                                    <p>{day.title}</p>
-                                                </Link>
-                                                <p>{day.hours.slice(0, -3)}*</p>
-                                            </td>
-                                        ))}
-                                    </tr>
-                                    <tr>
-                                        <th>Giovedì</th>
-                                        {this.state.calendar?.thu?.map((day, idx) => (
-                                            <td key={idx}>
-                                                <Link className='link-calendar' to={`/anime/details/${day.series}`}>
-                                                    <p>{day.title}</p>
-                                                </Link>
-                                                <p>{day.hours.slice(0, -3)}*</p>
-                                            </td>
-                                        ))}
-                                    </tr>
-                                    <tr>
-                                        <th>Venerdì</th>
-                                        {this.state.calendar?.fri?.map((day, idx) => (
-                                            <td key={idx}>
-                                                <Link className='link-calendar' to={`/anime/details/${day.series}`}>
-                                                    <p>{day.title}</p>
-                                                </Link>
-                                                <p>{day.hours.slice(0, -3)}*</p>
-                                            </td>
-                                        ))}
-                                    </tr>
-                                    <tr>
-                                        <th>Sabato</th>
-                                        {this.state.calendar?.sat?.map((day, idx) => (
-                                            <td key={idx}>
-                                                <Link className='link-calendar' to={`/anime/details/${day.series}`}>
-                                                    <p>{day.title}</p>
-                                                </Link>
-                                                <p>{day.hours.slice(0, -3)}*</p>
-                                            </td>
-                                        ))}
-                                    </tr>
-                                    <tr>
-                                        <th>Domenica</th>
-                                        {this.state.calendar?.sun?.map((day, idx) => (
-                                            <td key={idx}>
-                                                <Link className='link-calendar' to={`/anime/details/${day.series}`}>
-                                                    <p>{day.title}</p>
-                                                </Link>
-                                                <p>{day.hours.slice(0, -3)}*</p>
-                                            </td>
-                                        ))}
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col'>
-                            * Gli orari sono molto indicativi, tieni conto del tempo della traduzione da parte del fansub
-                        </div>
+                    } />
+                    <title>Calendario Uscite - AnimeAndromeda - Anime Andromeda</title>
+                    <link rel='canonical' href='https://www.animeandromeda.net' />
+                </Helmet>
+            </HelmetProvider>
+            <div className='mt-3'></div>
+            <div className='container home-anime-container p-3 shadow rounded'>
+                <div className='row'>
+                    <div className='col'>
+                        <h3 className='home-section-title'>{'Calendario Uscite'}</h3>
                     </div>
                 </div>
+                <div className='row'>
+                    <div className='col'>
+                        <Table responsive borderless className='anime-calendar'>
+                            <tbody>
+                                <tr>
+                                    <th>Lunedì</th>
+                                    {calendar?.mon?.map((day, idx) => (
+                                        <td key={idx}>
+                                            <Link className='link-calendar' to={`/anime/details/${day.series}`}>
+                                                <p>{day.title}</p>
+                                            </Link>
+                                            <p>{day.hours.slice(0, -3)}*</p>
+                                        </td>
+                                    ))}
+                                </tr>
+                                <tr>
+                                    <th>Martedì</th>
+                                    {calendar?.tue?.map((day, idx) => (
+                                        <td key={idx}>
+                                            <Link className='link-calendar' to={`/anime/details/${day.series}`}>
+                                                <p>{day.title}</p>
+                                            </Link>
+                                            <p>{day.hours.slice(0, -3)}*</p>
+                                        </td>
+                                    ))}
+                                </tr>
+                                <tr>
+                                    <th>Mercoledì</th>
+                                    {calendar?.wed?.map((day, idx) => (
+                                        <td key={idx}>
+                                            <Link className='link-calendar' to={`/anime/details/${day.series}`}>
+                                                <p>{day.title}</p>
+                                            </Link>
+                                            <p>{day.hours.slice(0, -3)}*</p>
+                                        </td>
+                                    ))}
+                                </tr>
+                                <tr>
+                                    <th>Giovedì</th>
+                                    {calendar?.thu?.map((day, idx) => (
+                                        <td key={idx}>
+                                            <Link className='link-calendar' to={`/anime/details/${day.series}`}>
+                                                <p>{day.title}</p>
+                                            </Link>
+                                            <p>{day.hours.slice(0, -3)}*</p>
+                                        </td>
+                                    ))}
+                                </tr>
+                                <tr>
+                                    <th>Venerdì</th>
+                                    {calendar?.fri?.map((day, idx) => (
+                                        <td key={idx}>
+                                            <Link className='link-calendar' to={`/anime/details/${day.series}`}>
+                                                <p>{day.title}</p>
+                                            </Link>
+                                            <p>{day.hours.slice(0, -3)}*</p>
+                                        </td>
+                                    ))}
+                                </tr>
+                                <tr>
+                                    <th>Sabato</th>
+                                    {calendar?.sat?.map((day, idx) => (
+                                        <td key={idx}>
+                                            <Link className='link-calendar' to={`/anime/details/${day.series}`}>
+                                                <p>{day.title}</p>
+                                            </Link>
+                                            <p>{day.hours.slice(0, -3)}*</p>
+                                        </td>
+                                    ))}
+                                </tr>
+                                <tr>
+                                    <th>Domenica</th>
+                                    {calendar?.sun?.map((day, idx) => (
+                                        <td key={idx}>
+                                            <Link className='link-calendar' to={`/anime/details/${day.series}`}>
+                                                <p>{day.title}</p>
+                                            </Link>
+                                            <p>{day.hours.slice(0, -3)}*</p>
+                                        </td>
+                                    ))}
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        * Gli orari sono molto indicativi, tieni conto del tempo della traduzione da parte del fansub
+                        </div>
+                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
 
 export default Calendar;
