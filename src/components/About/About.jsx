8b889e419e1d2036c5e188723497a8e0548ee4e9
Yun <marco.piovanello@gmail.com>
2021-05-06 15:16:34 +0200

    fix sitemap and updated About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -19,9 +19,15 @@ const Calendar = () => {
                     <div className='col text-left'>
                         <span>AnimeAndromeda è un piccolo progetto.</span>
                         <p> L'obbiettivo era di realizzare un sito di streaming anime
-                        veloce e soprattutto senza pubblicità (proprio senza).</p>
+                        veloce e soprattutto senza pubblicità.</p>
                         <p>Il codice è completamente opensource</p>
-                        <small>Fantastic Vectorized Padoru by <a href='https://www.deviantart.com/manaalchemist'>ManaAlchemist</a></small>
+                        <div>
+                            <a href="https://github.com/oppaoppai/animeandromeda-react">https://github.com/oppaoppai/animeandromeda-react</a>
+                        </div>
+                        <div>
+                            <a href="https://github.com/oppaoppai/animeandromeda-python">https://github.com/oppaoppai/animeandromeda-python</a>
+                        </div>
+                        {/* <small>Fantastic Vectorized Padoru by <a href='https://www.deviantart.com/manaalchemist'>ManaAlchemist</a></small> */}
                     </div>
                 </div>
             </div>
